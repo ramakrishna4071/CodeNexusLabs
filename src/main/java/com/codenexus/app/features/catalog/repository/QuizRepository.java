786365c4e9c6b63fa666002d4deab44194ee90f2
ramakrishna4071 <96335709+ramakrishna4071@users.noreply.github.com>
2026-05-05 10:17:21 +0530

    Delete src/main/java/com/codenexus/app/features/catalog/repository/QuizRepository.java

--- src/main/java/com/codenexus/app/features/catalog/repository/QuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codenexus.app.features.catalog.repository;
-
-import com.codenexus.app.features.catalog.entity.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-}
