0a138d7b552de9d6cb16ed01d0be6748ac3b0a10
ramakrishna4071 <96335709+ramakrishna4071@users.noreply.github.com>
2026-05-05 10:12:58 +0530

    Delete src/main/java/com/codenexus/app/config/JpaAuditingConfig.java

--- src/main/java/com/codenexus/app/config/JpaAuditingConfig.java
+++
@@ -1,9 +0,0 @@
-package com.codenexus.app.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing // This is the missing part!
-public class JpaAuditingConfig {
-}
